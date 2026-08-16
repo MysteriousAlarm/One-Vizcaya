38a5f4bad653945e6770fe0bdb7a22592c206c0e
Mysterious_Alarm <146867504+MysteriousAlarm@users.noreply.github.com>
2026-08-16 13:05:37 +0800

    To Copy gitignored files
    mkdir C:\Users\Administrator\.android -Force
    copy C:\Users\Administrator\Downloads\debug.keystore C:\Users\Administrator\.android\
    copy C:\Users\Administrator\Downloads\google-services.json android\app\
    copy C:\Users\Administrator\Downloads\firebase_options.dart lib\
    copy C:\Users\Administrator\Downloads\secrets.dart lib\
    copy C:\Users\Administrator\Downloads\key.properties android\
    copy C:\Users\Administrator\Downloads\upload-keystore.jks android\app\

---
+++ restore-secrets.bat
@@ -0,0 +1,7 @@
+mkdir C:\Users\Administrator\.android -Force
+copy C:\Users\Administrator\Downloads\debug.keystore C:\Users\Administrator\.android\
+copy C:\Users\Administrator\Downloads\google-services.json android\app\
+copy C:\Users\Administrator\Downloads\firebase_options.dart lib\
+copy C:\Users\Administrator\Downloads\secrets.dart lib\
+copy C:\Users\Administrator\Downloads\key.properties android\
+copy C:\Users\Administrator\Downloads\upload-keystore.jks android\app\
